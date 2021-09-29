98c40243963d5d764328c20678abb243be0cde5c
toki-debug <toki.debug@gmail.com>
2021-09-29 18:42:43 -0300

    FileIO

---
+++ Tecnicas de Programacao/FileIO.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    freopen("input.in","r",stdin);
+    freopen("output.out","w",stdout);
+
+    int a,b,c;
+    cin >> a >> b >> c;
+    cout << a+b+c << endl; 
+    return 0;
+}
