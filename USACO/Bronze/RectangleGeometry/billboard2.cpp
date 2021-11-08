bd59baf6e5b39a7ff493d2f8c31701cc80ef1c84
toki-debug <toki.debug@gmail.com>
2021-11-08 10:11:32 -0300

    USACO Guide Regtangle Geometry Updated

--- USACO/Bronze/RectangleGeometry/billboard2.cpp
+++ USACO/Bronze/RectangleGeometry/billboard2.cpp
@@ -1,21 +1,19 @@
-//Arissa Yoshida
-//Problem 1. Blocked Billboard II - http://www.usaco.org/index.php?page=viewproblem2&cpid=783
-
 #include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
 
-#define fast_io                                                                \
+using namespace std;
+#define MAX 31622777
+#define mod 1000000007
+
+#define no_tle_pls                                                             \
   ios::sync_with_stdio(false);                                                 \
   cin.tie(0);                                                                  \
   cout.tie(0);
-#define mod 1000000007
-#define MAX 100005
+typedef long long ll;
 
 void setIO(string s) {
   if (s.empty()) {
-    freopen("input.in", "r", stdin);
-    freopen("output.out", "w", stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
   } else {
     freopen((s + ".in").c_str(), "r", stdin);
     freopen((s + ".out").c_str(), "w", stdout);
@@ -23,33 +21,17 @@ void setIO(string s) {
 }
 
 int main() {
-  setIO("billboard");
-  fast_io int a, b, c, d;
-  int w, x, y, z;
-  cin >> a >> b >> c >> d;
-  cin >> w >> x >> y >> z;
-  ll r = 0;
-  int hor = (c - a) - max(0, min(c, y) - max(a, w));
-  int ver = (d - b) - max(0, min(d, z) - max(b, x));
-
-  if (ver == 0 && hor == 0) {
-    r = 0;
-  } else if (hor == 0) {
-    if (x > b && z < d) {
-      r = (c - a) * (d - b);
-    } else {
-      r = ver * (c - a);
-    }
-
-  } else if (ver == 0) {
-    if (w > a && y < c) {
-      r = (c - a) * (d - b);
-    } else {
-      r = hor * (d - b);
-    }
-
-  } else {
-    r = (c - a) * (d - b);
+  no_tle_pls
+      setIO("billboard");
+      int x1,
+      y1, x2, y2, a1, b1, a2, b2;
+  cin >> x1 >> y1 >> x2 >> y2;
+  cin >> a1 >> b1 >> a2 >> b2;
+  int area = abs(x1 - x2) * abs(y1 - y2);
+  if (x1 >= a1 && y1 >= b1 && x2 <= a2 && y2 <= b2) {
+    area = 0;
+  } else if ((x1 >= a1 && x2 <= a2 && !(b1>y1 && b2<y2)) || (y1 >= b1 && y2 <= b2 && !(a1>x1 && a2<x2))) {
+    area -= abs(max(x1,a1)-min(x2,a2)) * abs(max(y1,b1)-min(y2,b2));
   }
-  cout << r << endl;
+  cout << area << endl;
 }
